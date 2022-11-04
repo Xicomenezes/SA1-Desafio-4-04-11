515f49aa9bba6a9804235005b4716d48c74b05b2
Carlos Eduardo <108675199+Xicomenezes@users.noreply.github.com>
2022-11-04 13:27:48 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Testendereco
+{
+     class Endereco
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Insira o seu Nome da Empresa:");
+          Console.ReadLine();
+          Console.WriteLine("Insira seu CNPJ:");
+          Console.ReadLine();
+          Console.WriteLine("Iscrição Estadual:");
+          Console.ReadLine();
+          Console.WriteLine("Aguarde...");
+          (souPessoa == "juridica");
+           PessoaJuridica pj = new PessoaJuridica();
+           Endereco endereco = new Endereco();
+           endereco.Logradouro = "Rua rio úriau";
+           endereco.Numero = 44;
+           endereco.TipoEndereco = "Comercial";
+           pj.CNPJ = Console.ReadLine();
+          int NumDigit = 10;
+          Console.WriteLine("Logradouro:");
+          Console.ReadLine();
+          Console.WriteLine("Número:");
+          Console.ReadLine();
+          Console.WriteLine("Tipo do Endereço:");
+          Console.ReadLine();
+          Console.WriteLine("Você deseja tentar novamente : sim ou não?");
+          Console.ReadLine();  
+          
+        }
+    }
+}
